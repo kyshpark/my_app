7f8dd9a6ae2e896b2b589b63f2b8aa448be97a17
kyshpark <kysh.park@gmail.com>
2016-08-29 13:42:29 -0700

    readme.md added

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+// import modules
 var express = require('express');
 var path = require('path');
 var app = express();
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
+// connect database
 mongoose.connect(process.env.MONGO_DB);
 var db = mongoose.connection;
 db.once("open", function() {
@@ -12,64 +16,67 @@ db.on("error", function(err) {
   console.log("DB ERROR :", err);
 });
 
-var dataSchema = mongoose.Schema({
-  name:String,
-  count:Number
+// model setting
+var postSchema = mongoose.Schema({
+  title: {type:String, required:true},
+  body: {type:String, required:true},
+  createdAt: {type:Date, default:Date.now},
+  updatedAt: Date
 });
-var Data = mongoose.model('data', dataSchema);
-Data.findOne({name:"myData"}, function(err,data) {
-  if(err) return console.log("Data ERROR:", err);
-  if(!data){
-    Data.create({name:"myData",count:0}, function(err,data){
-      if(err) return console.log("Data ERROR:",err);
-      console.log("Counter initialized :",data);
-    });
-  }
-});
+var Post = mongoose.model('post',postSchema);
 
+// view setting
 app.set("view engine", "ejs");
+
+// set middlewares
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
-app.get('/', function(req, res) {
-  Data.findOne({name:"myData"},function(err,data){
-    if(err) return console.log("Data ERROR:",err);
-    data.count++;
-    data.save(function(err) {
-      if(err) return console.log("Data ERROR:",err);
-      res.render('my_first_ejs',data);
-    });
+// set routes
+app.get('/posts',function(req,res){
+  Post.find({}).sort('-createdAt').exec(function(err,posts){
+    if(err) return res.json({success:false, message:err});
+    res.render("posts/index", {data:posts});
   });
-});
-app.get('/reset', function(req, res) {
-  setCounter(res,0);
-});
-app.get('/set/count', function(req, res) {
-  if(req.query.count) setCounter(res,req.query.count);
-  else getCounter(res);
-});
-app.get('/set/:num', function(req, res) {
-  if(req.params.num) setCounter(res,req.query.count);
-  else getCounter(res);
-});
-function setCounter(res,num) {
-  console.log("setCounter");
-  Data.findOne({name:"myData"},function(err,data){
-    if(err) return console.log("Data ERROR:",err);
-    data.count=num;
-    data.save(function(err) {
-      if(err) return console.log("Data ERROR:",err);
-      res.render('my_first_ejs',data);
-    });
+}); // index
+app.get('/posts/new', function(req,res){
+  res.render("posts/new");
+}); // new
+app.post('/posts',function(req,res){
+  Post.create(req.body.post,function(err,post){
+    if(err) return res.json({success:false, message:err});
+    res.redirect('/posts');
   });
-}
-function getCounter(res) {
-  console.log("getCounter");
-  Data.findOne({name:"myData"},function(err,data){
-    if(err) return console.log("Data ERROR:",err);
-    res.render('my_first_ejs',data);
+}); // create
+app.get('/posts/:id',function(req,res){
+  Post.findById(req.params.id,function(err,post){
+    if(err) return res.json({success:false, message:err});
+    res.render("posts/show",{data:post});
   });
-}
+}); // show
+app.get('/posts/:id/edit',function(req,res){
+  Post.findById(req.params.id,function(err,post){
+    if(err) return res.json({success:false, message:err});
+    res.render("posts/edit",{data:post});
+  });
+}); // edit
+app.put('/posts/:id',function(req,res){
+  req.body.post.updatedAt=Date.now();
+  Post.findByIdAndUpdate(req.params.id,req.body.post,function(err,post){
+    if(err) return res.json({success:false, message:err});
+    res.redirect('/posts/'+req.params.id);
+  });
+}); // update
+app.delete('/posts/:id',function(req,res){
+  Post.findByIdAndRemove(req.params.id,function(err,post){
+    if(err) return res.json({success:false, message:err});
+    res.redirect('/posts');
+  });
+}); // destroy
 
+// start server
 app.listen(3000, function() {
   console.log('Server On!');
 });
